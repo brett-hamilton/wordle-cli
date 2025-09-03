df2abab16b728ab2279c8300ea2cab39a6b24394
Brett Hamilton <bretthamilton024@gmail.com>
2025-09-03 16:28:42 -0400

    added word guess prompt

--- driver.cpp
+++ driver.cpp
@@ -5,7 +5,17 @@ const std::string word = "trace";
 
 int main () {
 
-	std::cout << "The word is: " << word << "\n";
+	std::cout << "Guess: ";
+	std::string guess = "";
+	std::cin >> guess;
+
+	std::cout << "Your guess: " << guess << "\n";
+
+	// Game loop:
+	//	1. Show letters guessed
+	//	2. Prompt for word guess
+	//	3. Check letters
+	//	4. If correct guess, end; else, continue to #1
 
 	return 0;
 }
